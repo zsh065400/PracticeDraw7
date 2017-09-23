2252688fe620587e109c5fc6baeb49613e64e8f2
Scout <3104517435@qq.com>
2017-09-23 18:30:15 +0800

    complete

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice02/Practice02HsvEvaluatorLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice02/Practice02HsvEvaluatorLayout.java
@@ -3,6 +3,7 @@ package com.hencoder.hencoderpracticedraw7.practice.practice02;
 import android.animation.ObjectAnimator;
 import android.animation.TypeEvaluator;
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
@@ -48,10 +49,34 @@ public class Practice02HsvEvaluatorLayout extends RelativeLayout {
 
     private class HsvEvaluator implements TypeEvaluator<Integer> {
 
+        float[] startHsv = new float[3];
+        float[] endHsv = new float[3];
+        float[] outHsv = new float[3];
+
         // 重写 evaluate() 方法，让颜色按照 HSV 来变化
         @Override
         public Integer evaluate(float fraction, Integer startValue, Integer endValue) {
-            return startValue;
+
+            Color.colorToHSV(startValue, startHsv);
+            Color.colorToHSV(endValue, endHsv);
+
+            if (endHsv[0] - startHsv[0] > 180) {
+                endHsv[0] -= 360;
+            } else if (endHsv[0] - startHsv[0] < -180) {
+                endHsv[0] += 360;
+            }
+            outHsv[0] = startHsv[0] + fraction * (endHsv[0] - startHsv[0]);
+            if (outHsv[0] > 360) {
+                outHsv[0] -= 360;
+            } else if (outHsv[0] < 0) {
+                outHsv[0] += 360;
+            }
+
+            outHsv[1] = startHsv[1] + fraction * (endHsv[1] - startHsv[1]);
+            outHsv[2] = startHsv[2] + fraction * (endHsv[2] - startHsv[2]);
+
+            int alpha = startValue >> 24 + (int) ((fraction * (endValue >> 24 - startValue >> 24)));
+            return Color.HSVToColor(alpha, outHsv);
         }
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice06/Practice06KeyframeLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice06/Practice06KeyframeLayout.java
@@ -1,8 +1,13 @@
 package com.hencoder.hencoderpracticedraw7.practice.practice06;
 
+import android.animation.Keyframe;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
@@ -40,6 +45,14 @@ public class Practice06KeyframeLayout extends RelativeLayout {
                 // 结束帧：progress 回落到 80
                 // 使用 PropertyValuesHolder.ofKeyframe() 来把关键帧拼接成一个完整的属性动画方案
                 // 使用 ObjectAnimator.ofPropertyValuesHolder() 来创建动画
+                final Keyframe one = Keyframe.ofFloat(0, 0);
+                final Keyframe two = Keyframe.ofFloat(0.5f, 100);
+                final Keyframe three = Keyframe.ofFloat(1, 80);
+                final PropertyValuesHolder progress = PropertyValuesHolder.ofKeyframe("progress", one, two, three);
+                final ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(view, progress);
+                animator.setInterpolator(new AccelerateDecelerateInterpolator());
+                animator.setDuration(2000);
+                animator.start();
             }
         });
     }
